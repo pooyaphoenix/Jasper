569baebbc71db7235315994631f4a2c0e688dc78
ousmorez <morezinjast@gmail.com>
2019-05-26 10:43:10 +0430

    nothing

--- jasper/blog/views.py
+++ jasper/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
-    return render(request , 'blog/home.html')
+def index(request):
+    return HttpResponse('hi django!')
 

1fe13a23631a731143352d26ac6125d0d283f187
ousmorez <morezinjast@gmail.com>
2019-05-31 15:52:27 +0430

    show post on html

--- jasper/blog/views.py
+++ jasper/blog/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Book, User
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return render(request , 'home.html')
+
+      allof_books = Book.objects.all()
+
+      context = {
+            'allof_books' : allof_books
+      }
+
+      return render(request , 'home.html',context )
+
+def detail(request,book_id):
+
+
+      bookdetail = Book.objects.get(pk = book_id)
+      context = {
+            'bookdetail' : bookdetail,
+      }
+      return render (request , 'detail.html',context)
 
 

0bae37c83d7b74d347b5d72fe0b2e7358ff7b956
ousmorez <morezinjast@gmail.com>
2019-06-02 20:32:33 +0430

    making registeration

--- jasper/blog/views.py
+++ jasper/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
 from .models import Book
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserForm
 # Create your views here.
 def index(request):
 
@@ -22,4 +23,44 @@ def detail(request,book_id):
       }
       return render (request , 'detail.html',context)
 
+def jasper(request):
+      return render(request,'jasper.html')
 
+#صفحه ی ثبت نام
+def register(request):
+      if request.method == 'POST' :
+            form = UserForm(request.POST)
+            if form.is_valid():
+                  form.save()
+                  username = form.cleaned_data.get('username')
+                  messages.success(request , f'!ایجاد شد {username}  حساب کاربری')
+                  return redirect('index')
+      else:
+            form = UserForm()
+
+      
+
+
+      context = {
+            'form' : form,
+      }
+      return render(request , 'accounts/register.html',context)
+
+
+
+
+
+
+
+
+
+#def post(self, request):
+      #form = HomeForm(request.POST)
+      #if form.is_valid():
+            #text = form.cleaned_data['post']
+            #form = HomeForm()
+      #context = {
+            #'form':form , 'text': text
+      #}
+      
+      #return render(request, self.template_name , context)

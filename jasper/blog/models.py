5cdd01696e428e0b260ffb88d7f20a793a4f591c
ousmorez <morezinjast@gmail.com>
2019-07-05 13:24:02 +0430

    making ability to resize large pictures of profiles to making website not so heavy by adding PIL to models.

--- jasper/blog/models.py
+++ jasper/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 
 class Book(models.Model):
@@ -99,4 +99,14 @@ class profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300 :
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 

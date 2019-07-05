799419e528b64313a5f1abfe9b68158bbe958339
ousmorez <morezinjast@gmail.com>
2019-07-05 13:12:55 +0430

    adding ability to edit and update profiles and the user informations in databse

--- jasper/blog/forms.py
+++ jasper/blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Book
+from .models import Book , profile
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -23,3 +23,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ('name', 'master','field','university','status','status2','price','description')
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = profile
+        fields = ['image']
--- jasper/blog/views.py
+++ jasper/blog/views.py
@@ -3,7 +3,7 @@ from .models import Book,Universty,Field
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import userform2 , PostForm
+from .forms import userform2 , PostForm, UserUpdateForm, ProfileUpdateForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import redirect
@@ -74,7 +74,25 @@ def register(request):
 #پروفایل کاربری
 @login_required
 def myprofile(request):
-      return render(request, 'accounts/profile.html')
+      if request.method == 'POST':
+            u_form = UserUpdateForm(request.POST, instance=request.user)
+            p_form = ProfileUpdateForm(request.POST,
+            request.FILES, 
+            instance=request.user.profile,)
+            if u_form.is_valid() and p_form.is_valid():
+                  u_form.save()
+                  p_form.save()
+                  messages.success(request, f'حساب کاربری با موفقیت بروز رسانی شد.')
+                  return redirect('myprofile')
+      else:
+            u_form = UserUpdateForm(instance=request.user)
+            p_form = ProfileUpdateForm(instance=request.user.profile)
+
+      context = {
+            'u_form' : u_form,
+            'p_form' : p_form
+      }
+      return render(request, 'accounts/profile.html', context)
 
 #ثبت آگهی
 @login_required
